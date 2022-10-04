b3d21af84d81073562113a96c4f53f6fa5ccb817
hoge <hoge@hogehoge.com>
2022-10-04 23:11:31 +0900

    initial commit

---
+++ bomberman.py
@@ -0,0 +1,542 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+Usable Font search
+$ fc-list
+
+Enum class
+https://www.fenet.jp/dotnet/column/language/6708/
+
+"""
+
+import pygame
+from pygame.locals import *
+import random
+
+# field value
+CH_FREE = 0
+CH_FIRE = 1
+CH_HARD = 2
+CH_SOFT = 3
+CH_BOM = 4
+CH_ITEM = 5
+
+# item type
+ITEM_NONE = 0
+ITEM_BOM = 1
+ITEM_FIRE = 2 
+
+
+field = []
+
+bom_list = []
+fire_list = []
+block_list = []
+item_list = []
+
+# color value
+CL_FREE = (0,130,0)
+CL_HARD = (80,80,80)
+CL_SOFT = (95,95,95)
+
+# frame rate
+FPS = 50
+fpsClock = pygame.time.Clock()
+
+# game state transition wait
+STATE_TRANSITION_FRAME = 50
+state_transition_count = 0
+
+# window size
+WINDOW_W = 640
+WINDOW_H = 480
+CHIPSIZE = 32
+CHIPNUM_W = int(WINDOW_W/CHIPSIZE)-1
+CHIPNUM_H = int(WINDOW_H/CHIPSIZE)
+
+# game state
+ST_TITLE = 0
+ST_GAME = 1
+ST_GAMEOVER = 2
+
+# game constants
+BOM_TIMEOUT_FRAME = 100
+FIRE_FRAME = 30
+
+
+def print_field():
+    for f in field:
+        print(f)
+
+class Item():
+    def __init__(self, ch_x, ch_y, item_type):
+        self.ch_x = ch_x
+        self.ch_y = ch_y
+        self.dead = False
+        field[self.ch_y][self.ch_x] = CH_ITEM
+        self.item_type = item_type
+
+    def draw(self):
+        if self.item_type == ITEM_BOM:
+            item_surface_bom = mono_item_font.render("B", True, 'Red')
+        elif self.item_type == ITEM_FIRE:
+            item_surface_bom = mono_item_font.render("F", True, 'Red')
+        pygame.draw.rect(screen, (220,220,0), (self.ch_x * CHIPSIZE,self.ch_y * CHIPSIZE,32,32))
+        screen.blit(item_surface_bom, (self.ch_x * CHIPSIZE + 8,self.ch_y * CHIPSIZE))
+
+    def execute(self):
+        current = field[self.ch_y][self.ch_x]
+        if current == CH_FIRE:
+            self.dead = True
+            field[self.ch_y][self.ch_x] = CH_FREE
+
+    def get(self,px,py):
+        for i in item_list:
+            if i.ch_x == px and i.ch_y == py:
+                self.dead = True
+                field[py][px] = CH_FREE
+                return i.item_type
+
+
+
+class Block():
+    def __init__(self, ch_x, ch_y):
+        self.ch_x = ch_x
+        self.ch_y = ch_y
+        self.dead = False
+        field[self.ch_y][self.ch_x] = CH_SOFT
+        self.item_type = ITEM_NONE
+        
+    def containItem(self,item_type):
+        self.item_type = item_type
+
+    def execute(self):
+        # when block was broken by fire
+        px = self.ch_x
+        py = self.ch_y
+        if field[py][px] == CH_FIRE:
+            field[py][px] = CH_FREE
+            self.dead = True
+
+            if self.item_type:
+                item_list.append(Item(px,py,self.item_type))
+    
+    def draw(self):
+        pygame.draw.rect(screen, CL_SOFT, (self.ch_x * CHIPSIZE,self.ch_y * CHIPSIZE,32,32))
+
+class Bom():
+    def __init__(self, ch_x, ch_y, power):
+        self.ch_x = ch_x
+        self.ch_y = ch_y
+        field[self.ch_y][self.ch_x] = CH_BOM
+        self.timer = BOM_TIMEOUT_FRAME
+        self.dead = False
+        self.power = power
+        self.size_ratio = [1.0,0.9,0.8,0.9]
+    
+    def explode(self):
+        
+        dx = 0
+        dy = 0
+        
+        px = self.ch_x + dx
+        py = self.ch_y + dy
+        fire_list.append(Fire(px, py))
+        
+        for dx in range(1,self.power+1):
+            dy = 0
+            
+            px = self.ch_x + dx
+            py = self.ch_y + dy
+            
+            if not field[py][px] == CH_HARD:
+                if field[py][px] == CH_FREE:
+                    fire_list.append(Fire(px, py))
+                else:
+                    field[py][px] = CH_FIRE
+                    break
+            else:
+                break
+            
+        for dx in range(1,self.power+1):
+            dy = 0
+            
+            px = self.ch_x - dx
+            py = self.ch_y + dy
+            
+            if not field[py][px] == CH_HARD:
+                if field[py][px] == CH_FREE:
+                    fire_list.append(Fire(px, py))
+                else:
+                    field[py][px] = CH_FIRE
+                    break
+            else:
+                break
+
+        for dy in range(1,self.power+1):
+            dx = 0
+            
+            px = self.ch_x + dx
+            py = self.ch_y + dy
+            
+            if not field[py][px] == CH_HARD:
+                if field[py][px] == CH_FREE:
+                    fire_list.append(Fire(px, py))
+                else:
+                    field[py][px] = CH_FIRE
+                    break
+            else:
+                break
+            
+        for dy in range(1,self.power+1):
+            dx = 0
+            
+            px = self.ch_x + dx
+            py = self.ch_y - dy
+            
+            if not field[py][px] == CH_HARD:
+                if field[py][px] == CH_FREE:
+                    fire_list.append(Fire(px, py))
+                else:
+                    field[py][px] = CH_FIRE
+                    break
+            else:
+                break
+
+    def execute(self):
+        # timer overflow
+        self.timer -= 1
+        if self.timer < 0:
+            self.dead = True
+            self.explode()
+        # in an explosion
+        if field[self.ch_y][self.ch_x] == CH_FIRE:
+            self.dead = True
+            self.explode()
+        
+    def draw(self):
+        pygame.draw.rect(screen, CL_FREE, (self.ch_x * CHIPSIZE,self.ch_y * CHIPSIZE,32,32))
+        
+        pygame.draw.circle(
+            screen,
+            (0,0,0),
+            (self.ch_x * CHIPSIZE+CHIPSIZE/2,self.ch_y * CHIPSIZE+CHIPSIZE/2),
+            CHIPSIZE/2 * self.size_ratio[int(self.timer / 7 % 4)])
+
+class Fire():
+    def __init__(self, ch_x, ch_y):
+        self.ch_x = ch_x
+        self.ch_y = ch_y
+        field[self.ch_y][self.ch_x] = CH_FIRE
+        self.timer = 0
+        self.dead = False
+    
+    def execute(self):
+        self.timer += 1
+        if self.timer > FIRE_FRAME:
+            field[self.ch_y][self.ch_x] = CH_FREE
+            self.dead = True
+            
+    def draw(self):
+        bom_sound.play()
+        
+        pygame.draw.rect(screen, CL_FREE, (self.ch_x * CHIPSIZE,self.ch_y * CHIPSIZE,32,32))
+        pygame.draw.circle(
+            screen,
+            (255,0,0),
+            (self.ch_x * CHIPSIZE+CHIPSIZE/2,self.ch_y * CHIPSIZE+CHIPSIZE/2),
+            CHIPSIZE/2)
+        
+        
+class Player:
+    def __init__(self,ch_x, ch_y):
+        self.ch_x = ch_x #atari hantei position
+        self.ch_y = ch_y
+        # game variable
+        self.bom_power = 2
+        self.bom_stock = 2
+        # self.x = self.ch_x / CHIPSIZE
+        # self.y = self.ch_y / CHIPSIZE
+        self.dead = False
+
+    def execute(self, dx, dy, btn_space):
+        # death
+        if field[self.ch_y][self.ch_x] == CH_FIRE:
+            self.dead = True
+        # put a bom
+        if len(bom_list) < self.bom_stock:
+            if field[self.ch_y][self.ch_x] == CH_FREE:
+                if btn_space == True:
+                    bom_list.append(Bom(self.ch_x, self.ch_y,self.bom_power))
+        # walking
+        px = self.ch_x+dx
+        py = self.ch_y+dy
+        targ = field[py][px]
+        if not (targ == CH_HARD or targ == CH_SOFT or targ == CH_BOM):
+            self.ch_x += dx
+            self.ch_y += dy
+
+            # item get
+            if targ == CH_ITEM:
+                item_type = item_list[0].get(self.ch_x, self.ch_y)
+
+                if item_type == ITEM_FIRE:
+                    self.bom_power += 1
+                elif item_type == ITEM_BOM:
+                    self.bom_stock += 1
+        
+    def draw(self):
+        pygame.draw.circle(
+            screen,
+            (255,255,255),
+            (self.ch_x * CHIPSIZE + CHIPSIZE/2,self.ch_y * CHIPSIZE + CHIPSIZE/2),
+            0.8*CHIPSIZE/2
+            )
+
+
+
+
+
+
+
+def drawChip(type,x,y):
+    if type == CH_FREE:
+        pygame.draw.rect(screen, CL_FREE, (x,y,32,32))
+    elif type == CH_HARD:
+        pygame.draw.rect(screen, CL_HARD, (x,y,32,32))
+    elif type == CH_SOFT:
+        for b in block_list:
+            b.draw()
+    elif type == CH_BOM:
+        for b in bom_list:
+            b.draw()
+    elif type == CH_FIRE:
+        for f in fire_list:
+            f.draw()
+    elif type == CH_ITEM:
+        for i in item_list:
+            i.draw()
+
+
+
+def execute_list(obj_list):
+    for o in obj_list:
+        if o.dead == False:
+            o.execute()
+
+def delete_dead_from_list(obj_list):
+    while len(obj_list):
+        obj_list.sort(key= lambda x: x.dead == False)
+        if obj_list[0].dead == True:
+            obj_list.pop(0)
+        else:
+            break
+
+def isStateTransitionTimming(state_transition_count):
+    state_transition_count += 1
+    if state_transition_count > STATE_TRANSITION_FRAME:
+        state_transition_count = 0
+        return True
+    return False
+
+class GameClass:
+    def __init__(self):
+        # Object init
+        self.makeMap()
+        self.player = Player(1,1)
+        
+    def isContinue(self):
+        if self.player.dead == True:
+            return False
+        else:
+            return True
+
+    def makeMap(self):
+        field.clear()
+        item_list.clear()
+        fire_list.clear()
+        block_list.clear()
+        bom_list.clear()
+
+        for i in range(CHIPNUM_H):
+            if i == 0 or i == CHIPNUM_H-1:
+                field.append([CH_HARD] * CHIPNUM_W)
+            elif i % 2 == 1:
+                line = [CH_FREE] * CHIPNUM_W
+                line[0] = CH_HARD
+                line[CHIPNUM_W-1] = CH_HARD
+                field.append(line)
+            elif i % 2 == 0:
+                line = [CH_HARD] * CHIPNUM_W
+                for i in range(1,CHIPNUM_W,2):
+                    line[i] = CH_FREE
+                field.append(line)
+        
+        for i in range(10):
+            x = random.randint(2, CHIPNUM_W-1)
+            y = random.randint(2, CHIPNUM_H-1)
+            if field[y][x] == CH_FREE:
+                block_list.append(Block(x,y))
+
+        # Soft block randomly contains an item
+        contain_index = random.randint(0,len(block_list))
+        block_list[contain_index].containItem(ITEM_BOM)
+        block_list[contain_index+1].containItem(ITEM_FIRE)
+
+
+    def mainloop(self,running):
+        dx = 0
+        dy = 0
+        btn_space = False
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    dy -= 1
+                elif event.key == pygame.K_DOWN:
+                    dy += 1
+                elif event.key == pygame.K_LEFT:
+                    dx -= 1
+                elif event.key == pygame.K_RIGHT:
+                    dx += 1
+                elif event.key == pygame.K_SPACE:
+                    btn_space = True
+                elif event.key == pygame.K_d:
+                    print_field()
+                elif event.key == pygame.K_ESCAPE:
+                    running = False
+        
+        screen.fill((0,0,0))
+        
+        # field drawing
+        for y,line in enumerate(field):
+            for x,l in enumerate(line):
+                drawChip(l,x*CHIPSIZE,y*CHIPSIZE)
+        
+        # draw player
+        if self.player.dead == False:
+            self.player.execute(dx, dy, btn_space)
+            self.player.draw()
+
+        execute_list(fire_list)
+        execute_list(block_list)
+        execute_list(bom_list)
+        execute_list(item_list)
+        
+        # delete
+        delete_dead_from_list(fire_list)
+        delete_dead_from_list(block_list)
+        delete_dead_from_list(bom_list)    
+        delete_dead_from_list(item_list)
+        
+        pygame.display.flip()
+        fpsClock.tick(FPS)
+        
+        return running
+
+
+def st_game_loop(running,gamestate):
+
+    game = GameClass()
+    
+    while game.isContinue() and running:
+        running = game.mainloop(running)
+    
+    gamestate = ST_GAMEOVER
+
+    return running,gamestate
+
+
+def st_gameover_loop(running, gamestate):
+    btn_space = False
+    
+    screen.fill((200,100,100))
+
+    
+    title_surface = mono_font.render("GAMEOVER", True, 'Black')
+    sub_surface = mono_font.render("Hit space button", True, 'Black')
+    screen.blit(title_surface, (WINDOW_W/2-200,WINDOW_H/2))
+    screen.blit(sub_surface, (WINDOW_W/2-300,WINDOW_H/2+180))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                btn_space = True
+            elif event.key == pygame.K_ESCAPE:
+                running = False
+    
+    if btn_space == True:
+        pygame.time.wait(800)
+        gamestate = ST_TITLE
+    else:
+        gamestate = ST_GAMEOVER
+    
+    return running,gamestate
+
+def st_title_loop(running, gamestate):
+    btn_space = False
+    
+    screen.fill((100,100,100))
+
+    
+    title_surface = mono_font.render("BOMBERMAN", True, 'Black')
+    sub_surface = mono_font.render("Hit space button", True, 'Black')
+    screen.blit(title_surface, (WINDOW_W/2-300,WINDOW_H/2))
+    screen.blit(sub_surface, (WINDOW_W/2-300,WINDOW_H/2+180))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                btn_space = True
+            elif event.key == pygame.K_ESCAPE:
+                running = False
+    
+    if btn_space == True:
+        pygame.time.wait(800)
+        gamestate = ST_GAME
+    else:
+        gamestate = ST_TITLE
+    
+    return running,gamestate
+
+
+
+####################################################################
+
+pygame.init()
+
+screen = pygame.display.set_mode([WINDOW_W, WINDOW_H])
+pygame.display.set_caption("bomber man")
+clock = pygame.time.Clock()
+mono_font = pygame.font.Font("/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-R.ttf", 50)
+mono_item_font = pygame.font.Font("/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-R.ttf", 30)
+
+pygame.mixer.init(frequency = 44100, size = -16, channels = 2, buffer = 1024)
+bom_sound = pygame.mixer.Sound("explosion.wav")
+
+
+gamestate = ST_TITLE
+running = True
+while running:
+    
+    if gamestate == ST_TITLE:
+        running,gamestate = st_title_loop(running, gamestate)
+    elif gamestate == ST_GAME:
+        running,gamestate = st_game_loop(running, gamestate)
+    elif gamestate == ST_GAMEOVER:
+        running,gamestate = st_gameover_loop(running, gamestate)
+    
+    pygame.display.flip()
+    fpsClock.tick(FPS)
+
+pygame.quit()
+
